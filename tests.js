e56808bfdbfc9028007a8413f20397cd46fb40fc
Tony Luo <luo.tony37@gmail.com>
2015-08-10 14:20:28 -0700

    Try out environmental variables

--- tests.js
+++ tests.js
@@ -1,7 +1,13 @@
 var assert = require('chai').assert;
 
-describe('Simple Test', function() {
+describe('Simple test', function() {
   it('should equal 2', function() {
     assert.equal(1 + 1, 2);
-  })
+  });
+});
+
+describe('Environmental variable', function() {
+  it('should equal Tony', function() {
+    assert.equal(process.env.USER_NAME, 'Tony');
+  });
 });
